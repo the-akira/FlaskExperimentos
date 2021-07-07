b0b07169b1787e11e71952d8a50dd44bf30befc8
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-07 20:50:05 -0300

    LiveSearch atualizado

--- LiveSearch/static/search.js
+++ LiveSearch/static/search.js
@@ -1,25 +1,25 @@
 function live_search(value){
-	value = value.trim(); 
-	if(value != ""){ 
-		$.ajax({
-			url: "search",
-			data: {search_text: value},
-			dataType: "json",
-			success: function(data){
-				let res = "";
-				for(i in data.results){
-					res += 
-					`
-					<div>
-						${data.results[i]}
-					</div>
-					`;
-				}
-				$("#results").html(res);
-			}
-		});
-	}
-	else{
-		$("#results").html("");
-	}
+    value = value.trim(); 
+    if(value != "") { 
+        $.ajax({
+            url: "search",
+            data: {search_text: value},
+            dataType: "json",
+            success: function(data){
+                let res = "";
+                for(i in data.results){
+                    res += 
+                    `
+                    <div>
+                        ${data.results[i]}
+                    </div>
+                    `;
+                }
+                $("#results").html(res);
+            }
+        });
+    }
+    else {
+        $("#results").html("");
+    }
 }
