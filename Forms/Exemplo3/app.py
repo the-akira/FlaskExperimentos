2b5da31a2b47d1d01e8331e40ebdd0bea8e36957
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-08 01:02:12 -0300

    Formulário atualizado

--- Forms/Exemplo3/app.py
+++ Forms/Exemplo3/app.py
@@ -7,20 +7,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kmykey'
 
 class SimpleForm(FlaskForm):
-	name = StringField('Qual o seu nome?')
-	submit = SubmitField('Enviar')
+    name = StringField('Qual o seu nome?')
+    submit = SubmitField('Enviar')
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	form = SimpleForm()
+    form = SimpleForm()
 
-	if form.validate_on_submit():
-		session['name'] = form.name.data 
-		flash(f'Você alterou o seu nome para: {session["name"]}')
+    if form.validate_on_submit():
+        session['name'] = form.name.data 
+        flash(f'Você alterou o seu nome para: {session["name"]}')
 
-		return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
-	return render_template('index.html', form=form)
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
